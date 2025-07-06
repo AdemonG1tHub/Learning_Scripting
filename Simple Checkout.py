a0b755704bbd06ca901fd643633792bc108a609b
BendieGames <bendiereal@gmail.com>
2025-07-06 15:44:28 -0400

    Update Simple Checkout.py

--- Simple Checkout.py
+++ Simple Checkout.py
@@ -15,39 +15,41 @@ item3Price = "$5"
 item4 = "Discord Relay"
 item4Price = "$50"
 
-checkoutCompleted = False
+checkout_basket = []
 validAnswer = False
-checkout = ""
 
 # Display available items
-print(f"Here is what we have to offer:\n1. {item1}\n2. {item2}\n3. {item3}\n3. {item4}")
+print(f"Here is what we have to offer:\n1. {item1}\n2. {item2}\n3. {item3}\n4. {item4}")
 
 # Get user item selection
 def checkout():
     _ = os.system('cls')
-    print(f"Here is what we have to offer:\n1. {item1}\n2. {item2}\n3. {item3}\n3. {item4}")
-    answer = input("\nIf you'd like to order any of these, type the number of the first item you'd like to purchase, however if you want to complete your purchases then enter FINSH: ")
+    print(f"Here is what we have to offer:\n1. {item1}\n2. {item2}\n3. {item3}\n4. {item4}")
+    answer = input("\nIf you'd like to order any of these, type the number of the first item you'd like to purchase, however if you want to complete your purchases then enter EXIT: ")
+
     if answer == "1":
         print("Item 1 selected.")
-        checkout = f"\nYour basket has {item1} and costs {item1Price}"
+        checkout_basket.append(f"\nYour basket has {item1} and costs {item1Price}")
         checkout()
     elif answer == "2":
         print("Item 2 added to basket.")
-        checkout.append("\nYou added {item2} to your basket and costs {item2Price}")
+        checkout_basket.append(f"\nYou added {item2} to your basket and it costs {item2Price}")
         checkout()
     elif answer == "3":
         print("Item 3 selected.")
-        checkout.append("\nYour basket has {item3} and costs {item3Price}")
+        checkout_basket.append(f"\nYour basket has {item3} and costs {item3Price}")
         checkout()
     elif answer == "4":
         print("Item 4 selected.")
-        checkout.append("\nYour basket has {item4} and costs {item4Price}")
+        checkout_basket.append(f"\nYour basket has {item4} and costs {item4Price}")
         checkout()
     elif answer == "EXIT":
-        print(f'{checkout}')
+        print("Your current basket: ")
+        for item in checkout_basket:
+            print(item)
+        global validAnswer
         while not validAnswer:
             confirmation = input("\nWould you like to go ahead with this transaction? (Y/N): ").strip().upper()
-
             if confirmation == "Y":
                 print("Done! Your payment has been received!")
                 validAnswer = True
@@ -58,4 +60,7 @@ def checkout():
                 print("Invalid answer. Please type Y or N.\n")
     else:
         print("Invalid selection.")
+        checkout()
+
 checkout()
+
